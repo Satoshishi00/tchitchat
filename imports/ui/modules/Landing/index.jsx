9b969d697370b1b6bd68396f7abacd8f81c383b6
Satoshishi00 <webmaster-2.0@hotmail.com>
2019-07-04 16:40:11 +0200

    Modif des components Rooms et Landing

--- imports/ui/modules/Landing/index.jsx
+++ imports/ui/modules/Landing/index.jsx
@@ -1,42 +1,12 @@
-import React, { useState, useCallback } from "react";
-import { Accounts } from "meteor/accounts-base";
-import { Link } from "react-router-dom";
+import React from "react";
 
-import Fields from "./Fields";
-
-const Inscription = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [username, setUsername] = useState("");
-
-  const update = useCallback(
-    (e, { name, value }) => {
-      switch (name) {
-        case "email":
-          setEmail(value);
-          break;
-        case "password":
-          setPassword(value);
-          break;
-        case "username":
-          setUsername(value);
-          break;
-      }
-    },
-    [setEmail, setPassword, setUsername]
-  );
-
-  const signup = useCallback(() => {
-    Accounts.createUser({ email, password, username }, err => {
-      if (err) console.log(err);
-    });
-  }, [email, password, username]);
-
+const Landing = () => {
   return (
     <div>
       <h1>Landing</h1>
+      <p>Vous vous êtes perdus</p>
     </div>
   );
 };
 
-export default Inscription;
+export default Landing;
