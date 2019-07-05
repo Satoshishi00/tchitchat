642095f5ac27f0f5f7f7c3ce0860ae9a07d1ed29
Satoshishi00 <webmaster-2.0@hotmail.com>
2019-07-05 12:05:11 +0200

    add class 'responsive' to img in landing

--- imports/ui/modules/Landing/index.jsx
+++ imports/ui/modules/Landing/index.jsx
@@ -9,6 +9,7 @@ const Landing = () => {
         <Link to="rooms">Cliquer ici</Link> pour revenir sur les salons.
       </p>
       <img
+        class="responsive"
         src="https://images.unsplash.com/photo-1501511795728-df53825d742a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"
         alt="Qu'êtes vous allé chercher ? 😛"
       />
