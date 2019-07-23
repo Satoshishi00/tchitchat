3146bc00d3dd29ed9aa0393b00a147ab5496ef69
Satoshishi00 <webmaster-2.0@hotmail.com>
2019-07-23 14:51:18 +0200

    Listing des rooms

--- imports/ui/modules/Rooms/index.jsx
+++ imports/ui/modules/Rooms/index.jsx
@@ -1,14 +1,44 @@
-import React from "react";
 import { Link } from "react-router-dom";
+import { withTracker } from "meteor/react-meteor-data";
+import { Meteor } from "meteor/meteor";
+import React, { useState, useCallback } from "react";
+import CustomInput from "../../components/CustomInput";
 
-const Rooms = () => {
+import Rooms from "/imports/api/Rooms";
+
+import Loader from "/imports/ui/components/Loader";
+import Room from "./Room";
+
+const displayRooms = ({ userId, loading, rooms }) => {
+  const remove = useCallback(({ target: { id } }) => {
+    Meteor.call("rooms.remove", { id }, err => {
+      if (err) console.log(err);
+    });
+  }, []);
+
   return (
     <div>
       <h1>Rooms</h1>
       <a href="/rooms/add">Créer une room</a>
       <p>Listing des rooms</p>
+      <Loader
+        loading={loading}
+        render={rooms.map(room => (
+          <Room key={room._id} userId={userId} room={room} remove={remove} />
+        ))}
+      />
     </div>
   );
 };
 
-export default Rooms;
+export default withTracker(() => {
+  const roomsPublication = Meteor.subscribe("rooms.lasts");
+  const loading = !roomsPublication.ready();
+  const rooms = Rooms.find({}, { sort: { createdAt: -1 } }).fetch();
+  return {
+    userId: Meteor.userId(),
+    user: Meteor.user() || {},
+    loading,
+    rooms
+  };
+})(displayRooms);
